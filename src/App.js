156a478f1e2f393194f7221d5bc71737c6ef46b4
ukjunior <devmagnusuche@gmail.com>
2023-03-08 14:09:47 -0800

    added class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+let data = require("./data/util.json");
+
+// 1. Parse the JSON object to extract its structure.
+// 2. Iterate over the object's fields to generate the form's HTML code.
+// 3. Use the HTML code to render the form on a web page.
 
 function App() {
+  const [values, setValue] = useState(data.fields);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lesson with Mr Jeff</h1>
+      <form action="" className="form-section">
+        {values.map((item, index) => {
+          console.log("item :>> ", item);
+          const { label, maxLength, minLength, name, required, type, options } =
+            item;
+
+          return (
+            <>
+              <div key={index}>
+                <label for="scales">{label} </label>
+
+                <input  
+                  type={type}
+                  name={name}
+                  minLength={minLength}
+                  maxLength={maxLength}
+                  size = '15'
+                  required={required}
+                />
+                <br />
+              </div>
+              <div>
+                {item.options ? (
+                  <select>
+                    <option value="">--Please choose an option--</option>
+                    {options.map((item, index) => {
+                      const { label, value } = item;
+                      return (
+                        <>
+                          <option value={value} key={index}>
+                            {label}
+                          </option>
+                        </>
+                      );
+                    })}
+                  </select>
+                ) : (
+                  ""
+                )}
+              </div>
+            </>
+          );
+        })}
+      </form>
     </div>
   );
 }
 
 export default App;
+
